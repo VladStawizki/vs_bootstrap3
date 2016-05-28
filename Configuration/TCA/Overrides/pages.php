d6564de32d52bc24c53889b2462041bb03627ead
Vlad Stawizki <vlad.stawizki@gmail.com>
2016-05-28 17:33:40 +0200

    Bootstrap 3 Css for RTE

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,6 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'vs_bootstrap3',
+        'Configuration/PageTS/Default.txt',
+        'Bootstrap 3 Css in RTE'
+);
